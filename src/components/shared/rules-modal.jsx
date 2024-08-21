5a9402e87d6b4e470b0c2ab049af8a288097395a
Iuliia Ryseva <iulia.ryseva@gmail.com>
2024-08-22 02:19:09 +0300

    upd build

--- src/components/shared/rules-modal.jsx
+++ src/components/shared/rules-modal.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import styled from "styled-components";
 import { useSizeRatio } from "../../contexts/SizeRatioContext";
 import { Block } from "./block";
@@ -22,21 +23,49 @@ const ButtonStyled = styled(Button)`
     margin-top: ${({$ratio}) => $ratio * 20}px;
 `;
 
-export const RulesModal = ({ isFirstTime, onClose }) => {
+export const RulesModal = ({ isFirstTime, onClose, setIsRules }) => {
+    const [part, setPart] = useState(0);
     const ratio = useSizeRatio();
+
+    const handleNextClick = () => {
+        setPart(prev => prev + 1);
+        setIsRules(true);
+    }
+
+    const handleClick = () => {
+        setIsRules(false);
+        onClose();
+    }
 
     return (
         <Wrapper>
             <Content $ratio={ratio}>
                 <CommonText $ratio={ratio}>
-                    Вам нужно двигать блоки так, чтобы они расположились в порядке от 1 до 15.{'\n'}
-                    Для этого используйте пустую клетку — при нажатии на блок он переместится именно туда.{'\n'}{'\n'}
-                    После верной расстановки откроется послание от ЛАНИТ.{'\n'}{'\n'}
-                    <b>Обрати внимание:</b> при выходе из игры прогресс не сохраняется.
+                    {part === 0 ? (
+                        <>
+                            Вам нужно <b>двигать блоки</b> так, чтобы они расположились в порядке от 1 до 15.{'\n'}
+                            Для этого используйте пустую клетку — <b>при нажатии на блок</b> он переместится именно туда.{'\n'}{'\n'}
+                            После верной расстановки откроется <b>послание от ЛАНИТ</b>.
+                            Запоминайте фразы — они вам скоро пригодятся.{'\n'}{'\n'}
+                            <b>Обрати внимание:</b> при выходе из игры прогресс не сохраняется, поэтому лучше проходить игру за один раз.
+                        </>
+                    ): (
+                        <>
+                            Здесь находится <b>меню</b> — вы можете вернуться в него в любой момент,{' '}
+                            чтобы <b>посмотреть финальные фразы</b> пройденных уровней.{'\n\n'}
+                            Отсюда же можно будет <b>вернуться</b> в прогресс текущего уровня. Проходить уровни заново не потребуется.
+                        </>
+                    )}
                 </CommonText>
-                <ButtonStyled onClick={onClose} $ratio={ratio}>
-                    {isFirstTime ? 'Уровень 1' : 'Продолжить'}
-                </ButtonStyled>
+                {part === 0 ? (
+                    <ButtonStyled onClick={handleNextClick} $ratio={ratio}>
+                        Далее
+                    </ButtonStyled>
+                ): (
+                    <ButtonStyled onClick={handleClick} $ratio={ratio}>
+                        {isFirstTime ? 'Уровень 1' : 'Продолжить'}
+                    </ButtonStyled>
+                )}
             </Content>
         </Wrapper>
     )
